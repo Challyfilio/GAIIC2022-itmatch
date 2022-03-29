760609794976475410853cb629bd72e7f8f9cbb8
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2022-03-29 11:42:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 export PYTHONPATH="$PYTHONPATH:$PWD/src"
 export TOKENIZERS_PARALLELISM=false
+echo '* * * * * * *'
 
 python -u src/training/main.py \
-    --save-frequency 3   \
-    --train-data="src/data/train_coarse.txt,src/data/train_fine.txt.00"    \
-    --val-data="src/data/train_fine.txt.01"   \
-    --dataset-type="json" \
-    --warmup 1000  \
-    --batch-size=256  \
-    --lr=1e-4  \
-    --wd=0.1  \
-    --epochs=15  \
-    --workers=4   \
-    --dp   \
-    --multigpu 0 \
-    --name "demo3"
+  --save-frequency 2 \
+  --train-data="src/data/train_coarse.txt,src/data/train_fine.txt.00" \
+  --val-data="src/data/train_fine.txt.01" \
+  --dataset-type="json" \
+  --warmup 1000 \
+  --batch-size=128 \
+  --lr=1e-4 \
+  --wd=0.1 \
+  --epochs=20 \
+  --workers=4 \
+  --dp \
+  --multigpu 0 \
+  --name "demo1"
