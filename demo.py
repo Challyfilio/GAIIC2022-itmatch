e30f9e5a0af75b2c2ac868852e5ebf47fe78d59c
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2022-03-29 15:48:59 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -43,10 +43,10 @@ def load_model(checkpoint_path):
     
     return model.eval()
 
-checkpoint_path = "./logs/demo/checkpoints/epoch_60.pt"
+checkpoint_path = "./src/logs/demo1/checkpoints/epoch_6.pt"
 test_data = "./src/data/preliminary_testA.txt"
 attr_dict_file = "./src/data/attr_to_attrvals.json"
-out_file = "result.txt"
+out_file = "test_pred.txt"
 
 # build model
 model = load_model(checkpoint_path)
